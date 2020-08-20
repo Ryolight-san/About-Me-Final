b499a630cc72db8e21de902d544a78499ad3fcb7
Ryolight-san <69293999+Ryolight-san@users.noreply.github.com>
2020-08-20 02:11:25 -0400

    Javascript
    Javascript for About Me Website

---
+++ script.js
@@ -0,0 +1,40 @@
+function submitEmail(){
+  var message = document.getElementById("message");
+  var form = document.getElementById("email-form");
+
+  var email = document.getElementById("email");
+
+  if(email.value === ""){
+    message.innerHTML = "You need to put an email!"
+  }else{
+    form.style.display = "none";
+    message.innerHTML = "Thank you for submitting!"
+  }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
